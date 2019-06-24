c200938e6c2e071ea3c76e1ad6533f6cf1d8ee6e
TannorWallace <tannor.wallace@gmail.com>
2019-06-24 10:16:29 -0600

    last for the dAY

--- app/models/Song.js
+++ app/models/Song.js
@@ -1,7 +1,7 @@
 export default class Song {
     constructor(song) {
         this.title = song.trackName
-        //Change 250x250 if you want a larger image
+        // NOTE  Change 250x250 if you want a larger image
         this.albumArt = song.artworkUrl60.replace(/60x60/g, "250x250")
         this.artist = song.artistName
         this.collection = song.collectionName
@@ -9,17 +9,18 @@ export default class Song {
         this.preview = song.previewUrl
     }
 
-    // need a template use a Bootstrap card thing?
+    // REVIEW  need a template use a Bootstrap card thing?
     get Template() {
+        // FIXME  add popover
         return `
                 
-                 <div class="col-12 col-md-3 ">
+                 <div class="col-12 col-md-3  mb-1">
   
                     <div class="card border border-dark ">
                         <img class="center" src="${this.albumArt}" alt="Album Art"> 
                         <h5 class="card-title">Artist:${this.artist}</h5>
                         <h5 class="card-text ">Title:${this.title}</h5>
-                        <h5 class="card-text">Collection:${this.collection}</h5> 
+                        <h5 class="card-text text-truncate">Collection:${this.collection}</h5> 
                         <h5 class="card-text">Price:$${this.price}</h5>
                       Preview: <audio class="player" controls src="${this.preview}"></audio>
 
